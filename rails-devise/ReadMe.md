2e4d84beee48ce83fbc54599361d3ca6aaa0982b
zhaoli <zhaoace@gmail.com>
2013-07-23 06:19:50 +0800

    init with GEM 'devise'

---
+++ rails-devise/ReadMe.md
@@ -0,0 +1,16 @@
+ReadMe.md
+1. Create new application
+    # rails new rails-devise --skip-bundle
+
+2. Edit Gemfile
+    *. change the source to taobao
+        source 'http://ruby.taobao.org'
+    *. add devise
+        gem 'devise' 
+
+3. bundle install gem
+    # bundle install
+
+
+4. use devise ger the framework in this application
+    # rails generate devise:install
---
+++ rails-devise/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDevise::Application.config.secret_token = 'd78b60319a96f339c2ee3bf8270db15d3ed86f8039054d0195234b33db1ac83f8ec48b68b09c225d844cd1c25e42c74df74d00a9266c138f8a524472d8cdb2a8'
