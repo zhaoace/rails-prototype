5fdd9dd6c8416489a40a3b6f582b0f75a63f8cbf
zhaoli <zhaoace@gmail.com>
2013-07-30 07:49:56 +0800

    run pass the devise part

---
+++ devise-weibo-oa2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseWeiboOa2::Application.config.secret_token = 'd25d7acfd076036c68178ab5dbf0c3dba84f9013fad9bc2bdc76bae078f5537b2e4634e9ee149019e4ef606fb84d19c3f8540105e8be06fc6bbe3ebecf353a8c'
